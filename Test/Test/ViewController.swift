5200ec4965884866a59c25617dea1152fc2c9c8b
Daniel Frank <danjfrank08@gmail.com>
2019-06-24 16:04:26 -0500

    Updated Project Settings for iOS

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Daniel Frank on 6/24/19.
+//  Copyright © 2019 Daniel Frank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
